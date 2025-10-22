01c82efb3fdfd96de60ceb6adbcad51ff812f46f
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-10-22 10:59:46 -0300

    Add files via upload

---
+++ Atividade09/Atividade09/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] nomes = { "Ana", "Bruno", "Carlos", "Daniela", "Eduardo" };
+Console.Write("Digite um nome para procurar: ");
+string nomeBusca = Console.ReadLine();
+bool achou = false;
+for (int i = 0; i < nomes.Length; i++)
+{
+    if (nomes[i].ToLower() == nomeBusca.ToLower())
+    {
+        achou = true;
+        break;
+    }
+}
+if (achou == true)
+{
+    Console.WriteLine("O nome " + nomeBusca + " foi encontrado no vetor.");
+}
+else
+{
+    Console.WriteLine("O nome " + nomeBusca + " não está no vetor.");
+}
