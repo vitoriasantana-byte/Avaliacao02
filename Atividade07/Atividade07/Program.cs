89af9b24421d906c04d17d7162de667fe55cc1a9
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-10-22 10:54:25 -0300

    Add files via upload

---
+++ Atividade07/Atividade07/Program.cs
@@ -0,0 +1,24 @@
+﻿int start, end;
+
+Console.Write("Número inicial: "); 
+while (!int.TryParse(Console.ReadLine(), out start)) //início do while início
+    Console.Write("Entrada inválida. Digite um número inteiro para o inicial: ");
+
+Console.Write("Número final: ");
+while (!int.TryParse(Console.ReadLine(), out end))
+    Console.Write("Entrada inválida. Digite um número inteiro para o final: ");  //fim
+
+// Se o usuário digitou start > end, vai inverte pra evitar loop vazio
+if (start > end)
+{
+    int tmp = start;
+    start = end;
+    end = tmp;
+}
+
+for (int i = start; i <= end; i++)
+{
+    Console.WriteLine($"\nTabuada do {i}:");
+    for (int j = 1; j <= 10; j++)
+        Console.WriteLine($"{i} x {j} = {i * j}");
+}
