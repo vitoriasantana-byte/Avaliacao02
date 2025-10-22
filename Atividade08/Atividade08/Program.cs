1e7492f8c02bf16987e4bd985d9e2ec3eae1b9f2
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-10-22 10:58:29 -0300

    Add files via upload

---
+++ Atividade08/Atividade08/Program.cs
@@ -0,0 +1,31 @@
+﻿int voto, a = 0, b = 0, c = 0, nulos = 0;
+
+do
+{
+    Console.WriteLine("\nDigite seu voto:");
+    Console.WriteLine("1 - Candidato A");
+    Console.WriteLine("2 - Candidato B");
+    Console.WriteLine("3 - Candidato C");
+    Console.WriteLine("9 - Voto Nulo");
+    Console.WriteLine("0 - Encerrar votação");
+    voto = int.Parse(Console.ReadLine());
+
+    switch (voto)
+    {
+        case 1: a++; break;
+        case 2: b++; break;
+        case 3: c++; break;
+        case 9: nulos++; break;
+        case 0: break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+} while (voto != 0);
+
+Console.WriteLine("\nRESULTADO FINAL:");
+Console.WriteLine($"Candidato A: {a}");
+Console.WriteLine($"Candidato B: {b}");
+Console.WriteLine($"Candidato C: {c}");
+Console.WriteLine($"Votos Nulos: {nulos}");
