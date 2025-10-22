3dc64aa7a558c1a24fcf76e13b31db4b95cf74a1
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-10-22 10:53:16 -0300

    Add files via upload

---
+++ Atividade06/Atividade06/Program.cs
@@ -0,0 +1,27 @@
+﻿double n1, n2, n3, p1, p2, p3, media;
+
+Console.Write("Nota 1: ");
+n1 = double.Parse(Console.ReadLine());
+Console.Write("Peso 1: ");
+p1 = double.Parse(Console.ReadLine());
+
+Console.Write("Nota 2: ");
+n2 = double.Parse(Console.ReadLine());
+Console.Write("Peso 2: ");
+p2 = double.Parse(Console.ReadLine());
+
+Console.Write("Nota 3: ");
+n3 = double.Parse(Console.ReadLine());
+Console.Write("Peso 3: ");
+p3 = double.Parse(Console.ReadLine());
+
+media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+Console.WriteLine($"\nMédia Ponderada: {media:F2}");
+
+if (media >= 7)
+    Console.WriteLine("Aprovado");
+else if (media >= 5)
+    Console.WriteLine("Recuperação");
+else
+    Console.WriteLine("Reprovado");
